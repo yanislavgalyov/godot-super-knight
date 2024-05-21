b9d7858cbabca3ae299cc3cffaa2002ba41bba33
Yanislav Galyov <y.galyov@gmail.com>
2024-05-22 01:52:03 +0300

    tweak: game pauses during dialog

--- First Game/scripts/actionable.gd
+++ First Game/scripts/actionable.gd
@@ -1,9 +1,23 @@
 extends Area2D
 class_name Actionable
 
+const Balloon = preload("res://scenes/balloon.tscn")
+
 @export var dialogue_resource: DialogueResource
 @export var dialogue_start: String
 
+func _ready()-> void:
+	DialogueManager.dialogue_ended.connect(on_dialogue_ended)
+
 func action()-> void:
 	if dialogue_resource and dialogue_start:
-		DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
+		var balloon: Node = Balloon.instantiate()
+		get_tree().current_scene.add_child(balloon)
+		balloon.start(dialogue_resource, dialogue_start)
+		pause(true)
+
+func on_dialogue_ended(_resource: DialogueResource)-> void:
+	pause(false)
+
+func pause(should_pause: bool)-> void:
+	get_tree().paused = should_pause
--- First Game/scripts/game_manager.gd
+++ First Game/scripts/game_manager.gd
@@ -4,6 +4,10 @@ var score = 0
 
 @onready var score_label = $ScoreLabel
 
-func add_point():
+func add_point()-> void:
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins."
+
+func _physics_process(_delta)-> void:
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = !get_tree().paused
