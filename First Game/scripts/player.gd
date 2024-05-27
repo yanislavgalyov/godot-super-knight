1a4034f1830daa1b6e38a1450ee33b19266dd4c2
Yanislav Galyov <y.galyov@gmail.com>
2024-05-27 11:36:49 +0300

    feat: add coyote timer and wall jump timer to player.

--- First Game/scripts/player.gd
+++ First Game/scripts/player.gd
@@ -2,15 +2,25 @@ extends CharacterBody2D
 
 @onready var dash = $dash as Dash
 @export var player_stats: PlayerStats
+@export_range(0, 10) var coyote_frames: int = 4
+@export_range(0, 10) var wall_jump_frames: int = 4
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var physics_ticks_per_second = ProjectSettings.get_setting("physics/common/physics_ticks_per_second")
 
 @onready var animated_sprite = $AnimatedSprite2D as AnimatedSprite2D
 @onready var actionable_finder = $ActionableFinder as Area2D
+@onready var coyote_timer = $CoyoteTimer as Timer
+@onready var wall_timer = $WallTimer as Timer
+
+var was_wall_normal: Vector2
 
 func _ready()-> void:
 	# https://www.reddit.com/r/godot/comments/16bormn/comment/jzgsdyd/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
 	safe_margin = 0.1
+
+	coyote_timer.wait_time = float(coyote_frames) / physics_ticks_per_second
+	wall_timer.wait_time = float(wall_jump_frames) / physics_ticks_per_second
 
 func _physics_process(delta: float)-> void:
 	if Input.is_action_just_pressed("interact"):
@@ -30,18 +40,32 @@ func _physics_process(delta: float)-> void:
 	apply_friction(direction, delta)
 	apply_air_resistance(direction, delta)
 
+	var was_on_floor = is_on_floor()
+	var was_on_wall = is_on_wall_only()
+	if was_on_wall:
+		was_wall_normal = get_wall_normal()
+
 	move_and_slide()
+
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_timer.start()
+
+	# timer starts at the first frame the player is on_wall_only
+	if was_on_wall:
+		wall_timer.start()
+
 	update_animation(direction)
 
 func handle_actions(direction: float, delta: float)-> void:
 	# normal jump
-	if is_on_floor() and Input.is_action_just_pressed("jump"):
+	if (is_on_floor() or coyote_timer.time_left > 0.0) and Input.is_action_just_pressed("jump"):
 		velocity.y = player_stats.JUMP_VELOCITY
+		coyote_timer.stop()
 
 	# wall jump
-	if  is_on_wall_only() and Input.is_action_just_pressed("jump"):
-		var wall_normal: Vector2 = get_wall_normal()
-		if direction == wall_normal.x:
+	if  wall_timer.time_left > 0.0 and Input.is_action_just_pressed("jump"):
+		if direction == was_wall_normal.x:
 			velocity = Vector2(move_toward(velocity.x, player_stats.SPEED * direction, player_stats.ACCELERATION * delta), player_stats.JUMP_VELOCITY)
 
 	# dashing
