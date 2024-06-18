0dbe99fd74f95507cdc3d8401322e0195f5280d8
Yanislav Galyov <y.galyov@gmail.com>
2024-06-18 14:42:02 +0300

    fix: change viewport settings

--- First Game/scripts/autloloads/autoload_settings.gd
+++ First Game/scripts/autloloads/autoload_settings.gd
@@ -1,5 +1,5 @@
 extends Node
 
-func _init():
+func _init()-> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, true)
